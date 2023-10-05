32e149250acec6f8b7131f04cdf337cbe058619d
Pratik Bali <pratikbali96@gmail.com>
2023-10-05 18:28:57 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: BallPage(),
+  ));
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+
+  void ballButtonPressed () {
+    setState(() {
+      ballNumber = Random().nextInt(4) + 1;
+    });
+    print('Ball button presssed $ballNumber');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {ballButtonPressed();},
+      ),
+    );
+  }
+}
